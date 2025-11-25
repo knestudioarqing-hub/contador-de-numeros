78c7a47dbb1f0a8eccda04ac5f70436d69b82180
gian <gnproyectosarq@gmail.com>
2025-11-24 22:49:04 -0300

    jjs

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
     },
     plugins: [react()],
-    base: "https://knestudioarqing-hub.github.io/contador-de-numeros/",
+    base: "/contador-de-numeros/",
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
